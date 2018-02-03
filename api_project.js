0662f0e79ba85166a09ac419bbc3e169d57857c3
Menasi Ephrem <menasiephrem@gmail.com>
2018-02-03 17:38:33 +0300

    commit init

---
+++ api_project.js
@@ -0,0 +1,21 @@
+define({
+  "name": "Gebeya Networking Api",
+  "version": "1.0.0",
+  "description": "A simple networking api for Gebeya final exam",
+  "url": "https://networking-gebeya.herokuapp.com",
+  "order": [
+    "User",
+    "Profile",
+    "Match",
+    "Following"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-02-03T14:32:38.851Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
